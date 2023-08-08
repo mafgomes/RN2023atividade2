bfabcf2626a5e38ecbaee9f479617a6d1f430d52
Marcelo A F Gomes <marcelo.a.gomes@iesb.edu.br>
2023-08-08 01:41:40 -0300

    Melhorias ao hidekeys.sh

--- hidekeys.sh
+++ hidekeys.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+tmpfilename=$(mktemp $(basename $0).XXXXXX)
+
 while read tok
 do
-    find src -type f -exec echo "
-	if grep -l '$tok' {};
-	then
-		sed -e 's/$tok/[GITHUB TOKEN]/g' {} > x \
-		&& \
-		mv x {};
-	fi" \;
+	for dir in src android ios
+	do
+	    find "$dir" -type f -exec echo "
+		if grep -l '$tok' {};
+		then
+			sed -e 's/$tok/[GITHUB TOKEN]/g' {} > '$tmpfilename' \
+			&& \
+			mv '$tmpfilename' {};
+		fi" \;
+	done
 done < token.env | /bin/bash
+
+if [ -e "$tmpfilename" ]
+then
+	rm -f "$tmpfilename"
+fi
